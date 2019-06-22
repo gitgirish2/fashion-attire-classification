fadf162c5dc6edfc61feade8f353bceee78a83e4
Kumar Nityan Suman <nityan.suman@gmail.com>
2019-06-22 13:18:22 +0530

    Added path configuration

---
+++ configs.py
@@ -0,0 +1,15 @@
+""" 
+@Author: Kumar Nityan Suman
+@Date: 2019-06-22 12:38:25
+"""
+
+
+# Load packages
+import os
+
+
+# Path configuration
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) + "/"
+DATA_DIR = ROOT_DIR + "data/"
+LOG_DIR = ROOT_DIR + "log/"
+VIS_DIR = ROOT_DIR + "visualization/"
